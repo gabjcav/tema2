37cd2b1c8fcc051f471e8b5b86c216c22bc97775
steff-dz <69957357+steff-dz@users.noreply.github.com>
2020-10-09 15:20:18 +0200

    addes skate array

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+let skates = [
+    {
+        name: "Reidell Solaris",
+        type: "Roller Derby",
+        price: 3700,
+        picUrl: "./images/solaris.png"
+    },
+    {
+        name: "Bont Quad Star",
+        type: "Roller Derby",
+        price: 5400,
+        picUrl: "./images/bontquadstar.png"
+    },
+    {
+        name: "Chaya Emerald Soft",
+        type: "Roller Derby",
+        price: 1400,
+        picUrl: "./images/chayaemeraldsoft.jpg"
+    },
+    {
+        name: "Moxie Lolly",
+        type: "Park Skate",
+        price: 2700,
+        picUrl: "./images/lolly.png"
+    },
+    {
+        name: "Chaya Karma ",
+        type: "Park Skate",
+        price: 3500,
+        picUrl: "./images/chaya.jpg"
+    },
+    {
+        name: "Custom Van Skates",
+        type: "Park Skate",
+        price: 5000,
+        picUrl: "./images/vans.png"
+    },
+
+];
