4e587e08c0d9a2ca389a62b03866c4048357bdb1
steff-dz <69957357+steff-dz@users.noreply.github.com>
2020-10-10 11:53:14 +0200

    fixing up the skate container

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,60 @@
+const skateContainer = document.querySelector("#skate-container");
+//console.log(skateContainer);
+
+// skateContainer.innerHTML += `
+// <img src="./images/solaris.png">
+// `
+
 let skates = [
     {
         name: "Reidell Solaris",
         type: "Roller Derby",
         price: 3700,
-        picUrl: "./images/solaris.png"
+        picUrl: "./images/solaris.png",
+        descrip: "The Solaris boot from Reidell is made with cutting edge technology and design that re-defines performance and comfort standards for quad skating boots. They are comfortable, lightweight and stylish."
     },
     {
         name: "Bont Quad Star",
         type: "Roller Derby",
         price: 5400,
-        picUrl: "./images/bontquadstar.png"
+        picUrl: "./images/bontquadstar.png",
+        descrip: "Brought to you from Bont, this boot featuers a a thermoplastic heel, replaceable rubber bumper toe protection, an adjustable velcro strap and rear heel loop to loop laces through."
     },
     {
         name: "Chaya Emerald Soft",
         type: "Roller Derby",
         price: 1400,
-        picUrl: "./images/chayaemeraldsoft.jpg"
+        picUrl: "./images/chayaemeraldsoft.jpg",
+        descrip: "The perfect beginner boot that provides ventilation and extra padding for maximum comfort. It is also low cut for optimum maneurvability, and is highly durable."
     },
     {
-        name: "Moxie Lolly",
+        name: "Moxi Lolly",
         type: "Park Skate",
         price: 2700,
-        picUrl: "./images/lolly.png"
+        picUrl: "./images/lolly.png",
+        descrip: "Suede high top roller skates that can be used for indoor or outdoor skating. Moxi Lolly Skates are a mid-range lifestyle skate, designed for recreation and street skating."
     },
     {
         name: "Chaya Karma ",
         type: "Park Skate",
         price: 3500,
-        picUrl: "./images/chaya.jpg"
+        picUrl: "./images/chaya.jpg", 
+        descrip: "Perfect skatepark-ready skates that include Grind Blocks. The Grind Block is asymmetrical that opens up the skatepark to whole new array of tricks; further enhanced by the fact that you can choose to switch sides for added tricks and fun."
     },
     {
         name: "Custom Van Skates",
         type: "Park Skate",
         price: 5000,
-        picUrl: "./images/vans.png"
+        picUrl: "./images/vans.png",
+        descrip: "Bring your shoes and let us max out your vans to make the pefect street-park-jam-whatever you wish skate. The price includes plates, wheels, toe-stops, Chicks in Bowls side blocks, and installation. It does not include the van shoes."
     },
 
 ];
+
+skates.forEach(skate => {
+    skateContainer.innerHTML += `
+    <article>
+    <img src= ${skate.picUrl}>
+    </article>
+    `
+});
